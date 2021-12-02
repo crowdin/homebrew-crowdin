5e58cded17b530559d427afaaaa3bd1772e5ec74
Korbut Kirill <kirill.korbut@roobykon.com>
2021-12-02 02:07:17 +0200

    remove bottle :unneeded as it's deprecated

--- Formula/crowdin@2.rb
+++ Formula/crowdin@2.rb
@@ -3,9 +3,8 @@ class CrowdinAT2 < Formula
   homepage "https://support.crowdin.com/cli-tool/"
   url "https://downloads.crowdin.com/cli/v2/crowdin-cli-2.0.30.zip"
   sha256 "7066ce25339107c00b82918ef0c3b8d4913e3dedbafa677b2b50f12c45f6d4f8"
+  version "2.0.30"
 
-  bottle :unneeded
-  
   keg_only :versioned_formula
 
   depends_on "openjdk"
