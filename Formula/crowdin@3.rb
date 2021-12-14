0548e936aaca610a93e1a3f6cbc0f5abbf9fd78d
Andrii Bodnar <29282228+andrii-bodnar@users.noreply.github.com>
2021-12-14 14:50:19 +0200

    Version 3.7.3

--- Formula/crowdin@3.rb
+++ Formula/crowdin@3.rb
@@ -1,9 +1,9 @@
 class CrowdinAT3 < Formula
   desc "Crowdin CLI is a command-line tool that allows you to manage and synchronize localization resources with your Crowdin project."
   homepage "https://support.crowdin.com/cli-tool/"
-  url "https://downloads.crowdin.com/cli/v3/crowdin-cli-3.7.2.zip"
-  sha256 "ee9f838b819ccedc33c9b2537055e5ba7d7934561b24df1e1a6274cbd6e27f2d"
-  version "3.7.2"
+  url "https://downloads.crowdin.com/cli/v3/crowdin-cli-3.7.3.zip"
+  sha256 "da83586b70020aa8b5b43c060ba0106e76c2f9c8f1c02a68575f0c6ae500a895"
+  version "3.7.3"
 
   keg_only :versioned_formula
 
