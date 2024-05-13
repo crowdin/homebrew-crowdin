b07c2ce54a34d11ce6647f9f18ba2539ce802cac
Andrii Bodnar <andrii.bodnar@crowdin.com>
2024-05-13 15:29:00 +0300

    feat: CLI v4

---
+++ Formula/crowdin@4.rb
@@ -0,0 +1,25 @@
+class CrowdinAT3 < Formula
+  desc "Crowdin CLI is a command-line tool that allows you to manage and synchronize localization resources with your Crowdin project."
+  homepage "https://crowdin.github.io/crowdin-cli/"
+  url "https://github.com/crowdin/crowdin-cli/releases/download/4.0.0/crowdin-cli.zip"
+  sha256 "f5282106bfafc97af307eceaf89d2144c548a26e266f8c74c414aa4b812a8553"
+  version "4.0.0"
+
+  keg_only :versioned_formula
+
+  depends_on "openjdk"
+
+  def install
+    libexec.install "crowdin-cli.jar"
+    bin.write_jar_script libexec/"crowdin-cli.jar", "crowdin"
+  end
+
+  test do
+    generate_output = shell_output("#{bin}/crowdin init").chomp
+    assert_predicate testpath/"crowdin.yml", :exist?
+    assert_match /^Generating Crowdin CLI configuration skeleton .*crowdin\.yml\'- OK$/, generate_output
+    lint_output = shell_output("#{bin}/crowdin config lint", 1).split("\n")
+    assert_match /^`Source` parameter is empty$/, lint_output[0]
+    assert_match /^`Translation` parameter is empty$/, lint_output[1]
+  end
+end
